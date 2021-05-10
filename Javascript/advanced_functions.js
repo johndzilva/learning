afd57be7841f1785ad4f444a8b1b9544040831d6
John <johndesilva01@gmail.com>
2021-05-10 23:30:32 +0900

    Update advanced_functions.js

--- Javascript/advanced_functions.js
+++ Javascript/advanced_functions.js
@@ -30,13 +30,12 @@ curriedMultiply(5)(7);
 multiplyBy5(6);
 // Result: 30
 
-
 //----------
 //Compose
 //----------
-const compose = (f,g) => (a) => f(g(a));
+const compose = (f, g) => (a) => f(g(a));
 
 const sum = (num) => num + 1;
 
 compose(sum, sum)(5);
-// Result: 7  
+// Result: 7

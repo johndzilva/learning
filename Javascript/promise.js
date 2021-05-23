c9bd2741fab126b6992ba6712f6db27ced3a011d
John <johndesilva01@gmail.com>
2021-05-23 22:56:56 +0900

    Update promise.js

--- Javascript/promise.js
+++ Javascript/promise.js
@@ -20,9 +20,31 @@ const urls = [
 ]
 
 Promise.all(urls.map(url => {
-  fetch(url).then(resp => resp.json())
+  return fetch(url).then(resp => resp.json())
 })).then(result => {
   console.log(result[0])
   console.log(result[1])
   console.log(result[2])
 }).catch(() => console.log('error'))
+
+
+//----------------------
+// ES6 - Async
+//----------------------
+
+//The async and await keywords enable asynchronous, promise-based behavior 
+//to be written in a cleaner style, avoiding the need to explicitly configure promise chains.
+
+const getData = async function() {
+  try {
+    const [users, posts, albums] = await Promise.all(urls.map(url => {
+      return fetch(url).then(resp => resp.json())
+    }))
+    console.log(users)
+    console.log(posts)
+    console.log(albums) 
+  } catch (error) {
+    console.log('Error', error)
+  }
+}
+
